37c20485bc955639dbab34a6d8849881ec42af1b
Juda Kaleta <juda.kaleta@gmail.com>
2013-05-30 22:32:27 +0200

    LESS_OPTIONS config variable
    Config variable LESS_OPTIONS provide possibility of send
    options to less executable. It is very useful for compressing.
    
    Usage:
    LESS_OPTIONS = ['-O2', '-x', '--strict-imports']

--- less/settings.py
+++ less/settings.py
@@ -7,6 +7,7 @@ LESS_CACHE_TIMEOUT = getattr(settings, "LESS_CACHE_TIMEOUT", 60 * 60 * 24 * 30) 
 LESS_MTIME_DELAY = getattr(settings, "LESS_MTIME_DELAY", 10) # 10 seconds
 LESS_ROOT = getattr(settings, "LESS_ROOT", getattr(settings, "STATIC_ROOT", getattr(settings, "MEDIA_ROOT")))
 LESS_OUTPUT_DIR = getattr(settings, "LESS_OUTPUT_DIR", "LESS_CACHE")
+LESS_OPTIONS = getattr(settings, "LESS_OPTIONS", [])
 LESS_DEVMODE = getattr(settings, "LESS_DEVMODE", False)
 LESS_DEVMODE_WATCH_DIRS = getattr(settings, "LESS_DEVMODE_WATCH_DIRS", [settings.STATIC_ROOT])
 LESS_DEVMODE_EXCLUDE = getattr(settings, "LESS_DEVMODE_EXCLUDE", ())
